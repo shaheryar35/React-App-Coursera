bd9b38d62d1f862c11600e319e0f09b1212ac32f
shaheryar <shaheryarahmad35@gmail.com>
2019-12-11 12:05:18 +0500

    React Router

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,12 +1,54 @@
 import React,{Component} from 'react';
-import {Navbar, NavbarBrand,Jumbotron} from 'reactstrap';
+import {Navbar, NavbarBrand, Jumbotron, Nav, NavItem, Collapse, NavbarToggler } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 class Header extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            isNavOpen:false
+        };
+        this.toggleNav=this.toggleNav.bind(this);
+    }
+    toggleNav(){
+        this.setState({
+            isNavOpen:!this.state.isNavOpen
+        });
+    }
 render(){
 return(
 <>
-    <Navbar dark>
+    <Navbar dark expand="md">
         <div className="container">
-            <NavbarBrand href="/">Confusion Application</NavbarBrand>
+            <NavbarToggler onClick={this.toggleNav} />
+            <NavbarBrand href="/" classNam="mr-auto" >
+                <img src='assets/images/logo.png' width='41' height='30' alt='Ristorante Con Fusion' />
+            </NavbarBrand>
+            <Collapse isOpen={this.state.isNavOpen} navbar >
+            <Nav navbar>
+            <NavItem>
+                <NavLink className="nav-link" to="/home">
+                    <span className="fa fa-home fa-large"></span> Home
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink className="nav-link" to="/aboutus">
+                    <span className="fa fa-info fa-large"></span> About Us
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink className="nav-link" to="/menu">
+                    <span className="fa fa-list fa-large"></span> Menu
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink className="nav-link" to="/contactus">
+                    <span className="fa fa-address-card fa-large"></span> Contact Us
+                </NavLink>
+            </NavItem>
+
+
+            </Nav>
+            </Collapse>
         </div>
     </Navbar>
       <Jumbotron >
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,27 +2,34 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import {DISHES} from '../shared/dish';
 import DishDetail from './DishDetailComponent';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import {Switch, Route, Redirect } from 'react-router-dom';
 class Main extends Component {
 
    constructor(props){
      super(props)
      this.state={
        dishes:DISHES,
-       selectedDish:null
      };
-
    }
-   onDishSelected(dishID){
-    this.setState({selectedDish:dishID});
-}
   render() {
+    const HomePage=()=>{
+      return(
+          <Home />
+      );
+    }
     return (
       <div>
         <Header />
-      <Menu   dishes={this.state.dishes}  onClick={(dishID)=>this.onDishSelected(dishID)} />
-      <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> 
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes} />} />
+          <Redirect to ="/home" />
+
+        </Switch>
+     
       <Footer />
       </div>
     );
