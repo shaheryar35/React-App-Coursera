6332a6336cc0e9c0c1261cf2f637ed5cff4441e5
shaheryar <shaheryarahmad35@gmail.com>
2019-12-22 23:31:54 +0500

    Single Page Application Part-1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,19 +5,30 @@ import DishDetail from './DishDetailComponent';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactusComponent';
+import {COMMENTS} from '../shared/comments';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
 import {Switch, Route, Redirect } from 'react-router-dom';
+import { Form } from 'reactstrap';
 class Main extends Component {
 
    constructor(props){
      super(props)
      this.state={
        dishes:DISHES,
+       comments:COMMENTS,
+       promotions:PROMOTIONS,
+       leaders:LEADERS
      };
    }
   render() {
     const HomePage=()=>{
       return(
-          <Home />
+          <Home dish={this.state.dishes.filter((dish)=> dish.featured)[0]} 
+          promotion={this.state.promotions.filter((promo)=> promo.featured)[0]}
+          leader={this.state.leaders.filter((leader)=> leader.featured)[0]}
+          />
       );
     }
     return (
@@ -26,6 +37,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to ="/home" />
 
         </Switch>
